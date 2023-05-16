95892749b472f8a48ae11230ee7525d10753a57b
AbinMuds <abinashmudbhari@gmail.com>
2023-05-16 03:10:33 -0600

    changes in mapping

--- src/main/java/com/example/bookstoreproject/author/controller/AuthorController.java
+++ src/main/java/com/example/bookstoreproject/author/controller/AuthorController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/authors")
+@RequestMapping("api/v1/authors1")
 public class AuthorController {
     @Autowired
     private AuthorService authorService;
